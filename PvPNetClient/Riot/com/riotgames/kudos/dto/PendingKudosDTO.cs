9118f09aea45652e7367e747c0be7eaf9a4fb11b
Omdihar <Omdihar@web.de>
2016-10-10 19:57:21 +0200

    Add files via upload

---
+++ PvPNetClient/Riot/com/riotgames/kudos/dto/PendingKudosDTO.cs
@@ -0,0 +1,13 @@
+﻿using RtmpSharp.IO;
+using System;
+
+namespace LegendaryClient.Logic.Riot.Kudos
+{
+    [Serializable]
+    [SerializedName("com.riotgames.kudos.dto.PendingKudosDTO")]
+    public class PendingKudosDTO
+    {
+        [SerializedName("pendingCounts")]
+        public Int32[] PendingCounts { get; set; }
+    }
+}
