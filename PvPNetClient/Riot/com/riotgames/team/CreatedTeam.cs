a8bd504db041591a887f25d085bc754fcf777db6
Omdihar <Omdihar@web.de>
2016-10-10 19:59:22 +0200

    Add files via upload

---
+++ PvPNetClient/Riot/com/riotgames/team/CreatedTeam.cs
@@ -0,0 +1,16 @@
+﻿using RtmpSharp.IO;
+using System;
+
+namespace LegendaryClient.Logic.Riot.Team
+{
+    [Serializable]
+    [SerializedName("com.riotgames.team.CreatedTeam")]
+    public class CreatedTeam
+    {
+        [SerializedName("timeStamp")]
+        public Double TimeStamp { get; set; }
+
+        [SerializedName("teamId")]
+        public TeamId TeamId { get; set; }
+    }
+}
